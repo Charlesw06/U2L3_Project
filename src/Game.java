3c9b8b280e36cdf32e9e8fd2ab81a57151986a6e
BT_4N2_20 <cwalford3884@bths.edu>
2022-09-29 11:34:26 -0400

    Added Game class and SlopeCalculator class

---
+++ src/Game.java
@@ -0,0 +1,43 @@
+public class Game
+{
+    private int players;
+    private int score;
+    private boolean gameOver;
+
+    public Game()
+    {
+        players = 1;
+        score = 0;
+        gameOver = false;
+    }
+
+    public int getPlayers()
+    {
+        return players;
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public void addPlayer()
+    {
+        players ++;
+    }
+
+    public void increaseScore(int increase)
+    {
+        score += increase;
+    }
+
+    public double averageScorePerPlayer()
+    {
+        return (double) score / players;
+    }
+
+    public void end()
+    {
+        gameOver = true;
+    }
+}
--- src/U2L3Runner.java
+++ src/U2L3Runner.java
@@ -1,38 +1,13 @@
 public class U2L3Runner {
     public static void main(String[] args) {
-        int length, width, height;
-
-        length = 5;
-        width = 10;
-        height = length;
-
-        Box box1 = new Box(length, width, height);
-        Box box2 = new Box(length);
-        box1.printDimensions();
-        box2.printDimensions();
-
-        String bigger;
-
-        if (box1.volume() > box2.volume())
-        {
-            bigger = "box1 has greater volume";
-        }
-        else if (box1.volume() < box2.volume())
-        {
-            bigger = "box2 has greater volume";
-        }
-        else
-        {
-            bigger = "both boxes have equal volume";
-        }
-
-        System.out.println(bigger);
-
-        boolean box1sides = box1.anySideLongerThan(length);
-        boolean box2sides = box2.anySideLongerThan(length);
-
-        System.out.println("box1 has a side that exceeds " + length + ": " + box1sides);
-        System.out.println("box2 has a side that exceeds " + length + ": " + box2sides);
-
+        Game game = new Game();
+        game.addPlayer();
+        game.addPlayer();
+        game.increaseScore(2);
+        game.increaseScore(6);
+        System.out.println("There are " + game.getPlayers() + " in this game.");
+        System.out.println("The score is " + game.getScore() + ".");
+        System.out.println("The average score per player is " + game.averageScorePerPlayer() + ".");
+        game.end();
     }
 }
